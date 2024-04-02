94edbe136f5b3ad8852a117fe8991258adf9607d
foxymplayer <gufoxy194@gmail.com>
2024-04-02 12:49:54 -0300

    finalizado

--- calcular_salario.php
+++ calcular_salario.php
@@ -10,17 +10,17 @@
     <div class="container">
         <h1>Resultado</h1>
         <?php
-            // Recebendo os dados do formulário
+        
             $nome = $_POST["nome"];
             $meta_semanal = $_POST["meta_semanal"];
             $meta_mensal = $_POST["meta_mensal"];
 
-            // Calculando o salário
+            
             $salario_semanal = $meta_semanal * 200;
             $salario_mensal = ($meta_mensal >= $meta_semanal * 4) ? 1500 : 0;
             $salario_final = $salario_mensal + $salario_semanal;
 
-            // Exibindo o resultado
+         
             echo "<p>O salário final de $nome é R$" . number_format($salario_final, 2, ",", ".") . "</p>";
         ?>
     </div>
